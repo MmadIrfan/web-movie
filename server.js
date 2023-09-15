6af72940aef0bb6f01e7abad026404a648f69f11
Ghina Rodhotul Jannah <ghinarj12@gmai.com>
2023-09-15 14:05:16 +0700

    commit1_ghina

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+
+// Data film (contoh)
+const movies = [
+  { id: 1, title: 'Kisah Tanah Jawa Pocong Gundul', posterURL: 'link_gambar_1' },
+  { id: 2, title: 'Expend4bles', posterURL: 'link_gambar_2' },
+  { id: 3, title: 'Satu Hari Dengan Ibu', posterURL: 'link_gambar_2' },
+  { id: 4, title: 'Aku Tahu Kapan Kamu Mati - Desa Bunuh Diri', posterURL: 'link_gambar_2' },
+  { id: 5, title: 'A Haunting In Venice', posterURL: 'link_gambar_2' },
+  { id: 6, title: 'Retribution', posterURL: 'link_gambar_2' },
+  { id: 7, title: 'Talk To Me', posterURL: 'link_gambar_2' },
+  { id: 8, title: 'No Hard Feelings', posterURL: 'link_gambar_2' },
+  { id: 9, title: 'Air Mata Di Ujung Sajadah', posterURL: 'link_gambar_2' },
+  { id: 10, title: 'The Nun II', posterURL: 'link_gambar_2' },
+  // Tambahkan lebih banyak data film di sini
+];
+
+app.get('/api/v2/cineflex/movies', (req, res) => {
+  res.json(movies);
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server berjalan di port ${port}`);
+});
