d45ca70ae3b7f2541c1265d177824d43633fc474
Muhammad Irfan <Muhammad.irfan.ti.20@cic.ac.id>
2023-09-20 15:24:01 +0700

    mock api movies

--- server.js
+++ server.js
@@ -10,20 +10,70 @@ const users = [
 
 // Data film (contoh)
 const movies = [
-  { id: 1, title: 'Kisah Tanah Jawa Pocong Gundul', posterURL: 'link_gambar_1' },
-  { id: 2, title: 'Expend4bles', posterURL: 'link_gambar_2' },
-  { id: 3, title: 'Satu Hari Dengan Ibu', posterURL: 'link_gambar_2' },
-  { id: 4, title: 'Aku Tahu Kapan Kamu Mati - Desa Bunuh Diri', posterURL: 'link_gambar_2' },
-  { id: 5, title: 'A Haunting In Venice', posterURL: 'link_gambar_2' },
-  { id: 6, title: 'Retribution', posterURL: 'link_gambar_2' },
-  { id: 7, title: 'Talk To Me', posterURL: 'link_gambar_2' },
-  { id: 8, title: 'No Hard Feelings', posterURL: 'link_gambar_2' },
-  { id: 9, title: 'Air Mata Di Ujung Sajadah', posterURL: 'link_gambar_2' },
-  { id: 10, title: 'The Nun II', posterURL: 'link_gambar_2' },
+  {
+    id: 1,
+    title: "Kisah Tanah Jawa Pocong Gundul",
+    posterURL: "/poster/gundul.jpg",
+    label: "/label/su.png",
+  },
+  {
+    id: 2,
+    title: "Expend4bles",
+    posterURL: "/poster/expend4bles.jpg",
+    label: "/label/su.png",
+  },
+  {
+    id: 3,
+    title: "Satu Hari Dengan Ibu",
+    posterURL: "/poster/shdi.jpg",
+    label: "/label/su.png",
+  },
+  {
+    id: 4,
+    title: "Aku Tahu Kapan Kamu Mati - Desa Bunuh Diri",
+    posterURL: "/poster/atkkm.jpg",
+    label: "/label/su.png",
+  },
+  {
+    id: 5,
+    title: "A Haunting In Venice",
+    posterURL: "/poster/AHauntingInVenice.jpg",
+    label: "/label/su.png",
+  },
+  {
+    id: 6,
+    title: "Retribution",
+    posterURL: "/poster/RETRIBUTION.jpg",
+    label: "/label/su.png",
+  },
+  {
+    id: 7,
+    title: "Talk To Me",
+    posterURL: "/poster/TalkToMe.jpg",
+    label: "/label/su.png",
+  },
+  {
+    id: 8,
+    title: "No Hard Feelings",
+    posterURL: "/poster/NoHardFeelings.jpg",
+    label: "/label/su.png",
+  },
+  {
+    id: 9,
+    title: "Air Mata Di Ujung Sajadah",
+    posterURL: "/poster/amds.jpg",
+    label: "/label/su.png",
+  },
+  {
+    id: 10,
+    title: "The Nun II",
+    posterURL: "/poster/thenun2.jpg",
+    label: "/label/su.png",
+  },
   // Tambahkan lebih banyak data film di sini
 ];
 
-app.get('/api/v2/cineflex/movies', (req, res) => {
+app.get("/movies", (req, res) => {
   res.json(movies);
 });
 
@@ -40,6 +90,7 @@ app.get("/users", (req, res) => {
     res.status(401).json({ message: "Login failed. Invalid credentials." });
   }
 });
+
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
   console.log(`Server berjalan di port ${port}`);
